edfa0614204bb49ae7470fea915b2bd9a24746cc
vbonhomme <bonhomme.vincent@gmail.com>
2018-03-21 14:46:07 +0100

    initial release

---
+++ R/mevolCVP.R
@@ -0,0 +1,174 @@
+#######################
+# To cite the mevolCVP function: 
+# Evin, A., et al., The long and winding road: identifying pig domestication through molar size and shape, Journal
+#of Archaeological Science (2013), http://dx.doi.org/10.1016/j.jas.2012.08.005
+#######################
+
+
+
+########
+#mat: A PCA matrix (individuals in rows and variables in columns). 
+#group: the grouping factor (a vector or a factor)
+#lim: maximum number of components to be included
+#nrep: number of resampling. 
+#return.matrix, if =TRUE, the matrix used to draw the graphics is returned.
+#minInd, the minimum number of specimens to be included in the analyses. By default the number of specimens in the smallest group.
+#########
+
+
+mevolCVP<-function(mat=mat, group=group, lim=30, nrep=100, return.matrix=FALSE, minInd= NULL, print.legend=TRUE){
+  library(MASS)
+  mat<-as.matrix(mat)
+  group<-as.factor(as.character(group))
+  lgroup<-levels(group)
+  ngroup<-length(lgroup)
+  
+  if (is.null(minInd)) minInd <- min(table(group))
+  
+  a<-table(group); print(paste("The analyses are done with", ngroup, "groups")); print(a)
+  
+  CVoriginal<-vector(length=lim)
+  CVbalanced<-matrix(, nrow=nrep, ncol=lim)
+  CVrandom<-matrix(, nrow=nrep, ncol=lim)
+  CVbalancedrandom<-matrix(, nrow=nrep, ncol=lim)
+  
+  if (ncol(mat) != 1){
+    for (nbc in 2:lim) {
+      matr <- mat[,1:nbc]
+      z <- lda(matr, group, CV= TRUE)
+      tablez <- table(z$class, group)
+      CVoriginal[nbc] <- 100*sum(diag(tablez))/sum(tablez)
+    }
+  } 
+  
+  if (ncol(mat) == 1){
+    z <- lda(mat, group, CV= TRUE)
+    tablez <- table(z$class, group)
+    CVoriginal <- 100*sum(diag(tablez))/sum(tablez)
+  }
+  
+  for (j in 1:nrep){ 
+    
+    if (ncol(mat) != 1) {
+      matbalanced<-matrix(ncol=ncol(mat))
+      for (i in 1:ngroup){
+        matg<-mat[which(group==as.character(lgroup[i])),]
+        mati<-matg[sample(1:nrow(matg), minInd, replace=FALSE),]
+        matbalanced<-rbind(matbalanced, mati)
+      }
+      matbalanced<-prcomp(matbalanced[-1,])$x     
+    }
+    
+    
+    if (ncol(mat) == 1) {
+      matbalanced<-vector()
+      for (i in 1:ngroup){
+        matg<-mat[which(group==as.character(lgroup[i]))]
+        mati<-matg[sample(1:length(matg), minInd, replace=FALSE)]
+        matbalanced<-c(matbalanced, mati)
+      }
+      matbalanced<-as.matrix(matbalanced)
+    }
+    
+    gpbalanced<-rep(lgroup, each=minInd)
+    gprandom<-sample(group, length(group),replace=FALSE)
+    gpbalancedrandom<-sample(gpbalanced, length(gpbalanced),replace=FALSE)
+    if (ncol(mat) != 1){ 
+      for (nbc in 2:lim) {
+        #balanced samples
+        matr<-matbalanced[,1:nbc]
+        z <- lda(matr, gpbalanced, CV= TRUE)
+        tablez <- table(z$class, gpbalanced)
+        CVbalanced[j,nbc]<-100*sum(diag(tablez))/sum(tablez)
+        #balanced randomized samples
+        z <- lda(matr, gpbalancedrandom, CV= TRUE) 
+        tablez <- table(z$class, gpbalancedrandom)
+        CVbalancedrandom[j,nbc]<-100*sum(diag(tablez))/sum(tablez)
+        #un balanced randomized samples
+        z <- lda(mat[,1:nbc], gprandom, CV= TRUE)
+        tablez <- table(z$class, gprandom)
+        CVrandom[j,nbc]<-100*sum(diag(tablez))/sum(tablez)
+       }   
+    }
+    if (ncol(mat) == 1){ 
+      #balanced samples
+      z <- lda(matbalanced, gpbalanced, CV= TRUE)
+      tablez <- table(z$class, gpbalanced)
+      CVbalanced[j]<-100*sum(diag(tablez))/sum(tablez)
+      #balanced randomized samples
+      z <- lda(matbalanced, gpbalancedrandom, CV= TRUE) 
+      tablez <- table(z$class, gpbalancedrandom)
+      CVbalancedrandom[j]<-100*sum(diag(tablez))/sum(tablez)
+      #un balanced randomized samples
+      z <- lda(as.matrix(mat), gprandom, CV= TRUE)
+      tablez <- table(z$class, gprandom)
+      CVrandom[j]<-100*sum(diag(tablez))/sum(tablez)
+    }   
+  } 
+  if (ncol(mat) != 1){
+    CVoriginal<-CVoriginal[-1]
+    CVbalanced<-CVbalanced[,-1]
+    CVrandom<-CVrandom[,-1]
+    CVbalancedrandom<-CVbalancedrandom[,-1]
+  }
+  if (ncol(mat) != 1){  
+    par(mfrow=c(1,1))
+    CVbalanceds<-matrix(, lim-1, 3)
+    CVrandoms<-matrix(, lim-1, 3)
+    CVbalancedrandoms<-matrix(, lim-1, 3)
+    for (i in 1:(lim-1)) {
+      CVbalanceds[i,]<-c(mean(CVbalanced[,i]), quantile(CVbalanced[,i], c(0.05, 0.95))[1:2])
+      CVrandoms[i,]<-c(mean(CVrandom[,i]), quantile(CVrandom[,i], c(0.05, 0.95))[1:2])
+      CVbalancedrandoms[i,]<-c(mean(CVbalancedrandom[,i]), quantile(CVbalancedrandom[,i], c(0.05, 0.95))[1:2])
+    }
+    plot(x=c(2:lim), y=CVoriginal, type="l", lwd=2, col="white", tck=0, ylim=range(0:100), axes=FALSE, xlab= "Number of components", ylab= "%", xlim=range(2,lim))
+    title(main=paste("mevolCVP computed with ", nrep, " resampling, ", "\n",ngroup, " groups of ", minInd, " ind. min",sep=""))
+    axis(1, at=c(2:lim))
+    axis(2, at=c(0, 20, 40, 60, 80, 100))
+    box()
+    abline(h=c(0, 20, 40, 60, 80, 100), v = c(2:lim), col = "grey", lty = "dotted")
+    polygon(c(2:lim, lim:2), c(CVbalancedrandoms[,2], rev(CVbalancedrandoms[,3])), col=rgb(255, 255, 0, maxColorValue =255, alpha=90), xlim=range(2,lim))
+    par(new= TRUE)
+    plot(x=c(2:lim), y=CVbalancedrandoms[,1], type="l", lty="solid", lwd=2, col=rgb(139, 139, 0, maxColorValue =255, alpha=100), tck=1, ylim=range(0:100), axes=FALSE, xlab="", ylab="", xlim=range(2,lim)) 
+    polygon(c(2:lim, lim:2), c(CVrandoms[,2], rev(CVrandoms[,3])), col=rgb(0, 205, 0, maxColorValue =255, alpha=90), xlim=range(2,lim))
+    par(new= TRUE)
+    plot(x=c(2:lim), y=CVrandoms[,1], type="l", lty="solid", lwd=2, col=rgb(0, 139, 0, maxColorValue =255, alpha=100), tck=1, ylim=range(0:100), axes=FALSE, xlab="", ylab="", xlim=range(2,lim))
+    polygon(c(2:lim, lim:2), c(CVbalanceds[,2], rev(CVbalanceds[,3])), col=rgb(255, 165, 0, maxColorValue =255, alpha=90), xlim=range(2,lim))
+    par(new= TRUE)
+    plot(x=c(2:lim), y=CVbalanceds[,1], type="l", lty="solid", lwd=2, col=rgb(255, 140, 0, maxColorValue =255, alpha=100), tck=1, ylim=range(0:100), axes=FALSE, xlab="", ylab="", xlim=range(2,lim))
+    par(new= TRUE)
+    plot(x=c(2:lim), y=CVoriginal, type="l", lwd=2, col="red", tck=1, ylim=range(0:100), axes=FALSE, xlab="", ylab="", xlim=range(2,lim))
+    if (print.legend==TRUE) legend(2,20,c("CVoriginal", "CVbalanced","CVrandom", "CVbalancedrandom" ),col=c("red","orange", "green", "yellow"),lty=1,lwd=2, bty="o")
+  }
+  
+  if (ncol(mat) == 1){  
+    par(mfrow=c(1,1))
+    CVbalanceds<-c(mean(CVbalanced[,1]), quantile(CVbalanced[,1], c(0.05, 0.95))[1:2])
+    CVrandoms<-c(mean(CVrandom[,1]), quantile(CVrandom[,1], c(0.05, 0.95))[1:2])
+    CVbalancedrandoms<-c(mean(CVbalancedrandom[,1]), quantile(CVbalancedrandom[,1], c(0.05, 0.95))[1:2]) 
+    plot(x=2, y=CVoriginal, type="l", lwd=2, col="white", ylim=range(0:100), xlab="Samples", ylab= "%", xlim=range(0,3), axes="FALSE")
+    title(main=paste("mevolCVP computed with ", nrep, " resampling, ", "\n",ngroup, " groups of ", minInd, " ind. min",sep=""))
+    
+    axis(1, c(1,2),labels=c("a", "b")); axis(2); box()
+    abline(h=c(0, 20, 40, 60, 80, 100), v = c(1,2), col = "grey", lty = "dotted")
+    lines(c(2,2), c(CVbalancedrandoms[2], CVbalancedrandoms[3]), col=rgb(255, 255, 0, maxColorValue =255, alpha=90), xlim=range(0,3), lwd=5)
+    par(new= TRUE)
+    points(x=c(2,2,2), y=c(CVbalancedrandoms[1], CVbalancedrandoms[2], CVbalancedrandoms[3]), pch=20, cex=2, col=rgb(139, 139, 0, maxColorValue =255, alpha=100),  ylim=range(0:100), xlim=range(0,3)) 
+    lines(c(1,1), c(CVrandoms[2], CVrandoms[3]), col=rgb(0, 205, 0, maxColorValue =255, alpha=90), xlim=range(2,lim), lwd=5)
+    par(new= TRUE)
+    points(x=c(1,1,1), y=c(CVrandoms[1],CVrandoms[2], CVrandoms[3]), pch=20, cex=2,  col=rgb(0, 139, 0, maxColorValue =255, alpha=100), ylim=range(0:100), xlim=range(0,3))
+    lines(c(2,2), c(CVbalanceds[2], CVbalanceds[3]), col=rgb(255, 165, 0, maxColorValue =255, alpha=90), xlim=range(2,lim), lwd=5)
+    par(new= TRUE)
+    points(x=c(2,2,2), y=c(CVbalanceds[1], CVbalanceds[2], CVbalanceds[3]), pch=20, cex=2, col=rgb(255, 140, 0, maxColorValue =255, alpha=100), ylim=range(0:100), xlim=range(0,3))
+    par(new= TRUE)
+    points(x=1, y=CVoriginal, pch=20, cex=2, col="red", ylim=range(0:100), xlab= "Number of components", ylab= "%", xlim=range(0,3))
+    if (print.legend==TRUE) legend(0,20,c("CVoriginal", "CVbalanced","CVrandom", "CVbalancedrandom" ),col=c("red","orange", "green", "yellow"),lty=1,lwd=2, bty="o")
+  }
+  if (return.matrix==TRUE)
+  	names(CVoriginal)<-paste(1:length(CVoriginal)+1, "PCs", sep="")
+  	colnames(CVbalanced)<-colnames(CVbalancedrandom)<-colnames(CVrandom)<-paste(1:ncol(CVbalanced)+1, "PCs", sep="")
+  	CVsummary<-cbind(CVbalanceds,  CVrandoms, CVbalancedrandoms)
+  	colnames(CVsummary)<-paste( rep(c("mean-", "CI5%-", "CI95%-"),3), rep(c("CVbalanced", "CVrandom", "CVbalancedrandom"), each=3),sep="")
+  	rownames(CVsummary)<-paste(1:nrow(CVsummary)+1, "PCs", sep="")
+  	return(list(CVoriginal=CVoriginal, CVbalanced=CVbalanced, CVrandom=CVrandom, CVbalancedrandom=CVbalancedrandom, CVsummary=CVsummary))
+}
---
+++ R/pldam.R
@@ -0,0 +1,77 @@
+###pldam allows to perform predictive discriminant analyses based on balanced samples.
+# mat= matrix with the 'known' data (usually PC scores, but works also with univariate data)
+# group= grouping factor of the 'known' data
+# mattoid= matrix with the data to be predicted
+# codetoid= label of the specimens to predict
+# limproba= a limit for the posterior probability to be concidered
+# limCVP= a limit for the cross validation % of the discriminate analysis to be concidered
+# nrep= number of replicates
+# minInd= the number of specimens to include for the balanced design if smaller than the smallest of the "known" groups
+
+
+#$matplda provide a summary of the identification
+
+pldam<-function(mat=mat, group=group, mattoid=mattoid, codetoid=codetoid, ncpts=20,limproba=0, limCVP=FALSE, nrep=100, return.matrix=TRUE, minInd= NULL){
+	library(MASS)
+	mat<-as.matrix(mat)
+	mattoid<-as.matrix(mattoid)
+	group<-as.factor(as.character(group))
+	codetoid<-as.factor(codetoid)
+	lgroup<-levels(group)
+	ngroup<-length(lgroup)
+	
+	if (is.null(minInd)) minInd <- min(table(group))
+	
+	a<-table(group); print(paste("The analyses are done with", ngroup, "groups")); print(a)
+	print(paste(nrep, " ressampling"))
+	CVbalanced<-vector(length=nrep)
+	resultplda<-as.character(codetoid)
+	resultpldap<-as.character(codetoid)
+	print(paste("The limit % to concider an identification is ", limproba))
+	####
+	for (k in 1:nrep){	
+		###calcul mat avec effectifs balanc?s	
+		matbalanced<-matrix(ncol=ncol(mat), nrow=(minInd*ngroup))
+		for (i in 1:ngroup){
+				matg<-as.matrix(mat[which(group==as.character(lgroup[i])),])
+				mati<-as.matrix(matg[sample(1:nrow(matg), minInd, replace=FALSE),])
+				matbalanced[c((i*minInd-minInd+1):(i*minInd)),]<-mati
+		}
+		gpbalanced<-rep(lgroup, each=minInd)
+		##Calcul des prediction		
+			#balanced samples
+		matr<-as.matrix(matbalanced[,1:ncpts])
+		colnames(mattoid)<-colnames(matr)
+		z <- lda(matr, gpbalanced)
+		zp<-predict(z, as.matrix(mattoid[,1:ncpts]))
+		maxproba<-apply(zp$post, 1, max)
+		resultplda<-cbind(resultplda,as.character(zp$class))
+		resultpldap<-cbind(resultpldap,	round(maxproba,2))
+		z <- lda(matr, gpbalanced, CV=TRUE); a<-table(z$class, gpbalanced)
+		CVbalanced[k]<-sum(diag(a))/sum(a)
+	}
+	
+	if (limCVP ==FALSE) limCVP<-summary(CVbalanced)[5]
+	resultplda<-resultplda[,-1]
+	resultpldap<-resultpldap[,-1]
+	resultpldaCVP<-as.matrix(resultplda[,which(CVbalanced>=limCVP)])
+	resultpldapCVP<-as.matrix(resultpldap[,which(CVbalanced>=limCVP)])
+	print(paste("Results are provided for the", ncol(resultpldaCVP), "LDAs that have a CVP above ", limCVP))
+	matplda<-matrix(ncol=6, nrow=length(codetoid))
+	for (i in 1:length(codetoid)) {
+		matplda[i,1]<-as.character(codetoid[i])
+		tableau<-sort(table(resultpldaCVP[i, which(resultpldapCVP[i,]>=limproba)]), decreasing=TRUE)
+		if (length(tableau !=0)){
+		matplda[i,2]<- length(resultpldapCVP[i,which(resultpldapCVP[i,]>=limproba)])
+		matplda[i,3]<-names(tableau)[1]
+	  matplda[i,4]<-round(tableau[1]*100/sum(tableau),2)
+		if (length(tableau) != 1 ) matplda[i,5]<-names(tableau)[2]
+		if (length(tableau) != 1 ) matplda[i,6]<-round(tableau[2]*100/sum(tableau),2)
+	}
+	}	
+	rownames(resultplda)<-rownames(resultpldap)<-codetoid
+	colnames(resultplda)<-colnames(resultpldap)<-paste("rep", c(1:nrep), sep="")
+	colnames(matplda)<-c("codetoid", "NumberOfLda", "ID-1", "Proba-1", "ID-2", "Proba-2")
+	
+	if (return.matrix==TRUE)return(list(CVbalanced=CVbalanced, resultplda= resultplda, resultpldap= resultpldap, matplda=matplda))
+}
