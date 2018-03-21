5bc16652684f1951ec2b3c2025d5b05d1199d5bf
vbonhomme <bonhomme.vincent@gmail.com>
2018-03-21 15:24:02 +0100

    initial build

---
+++ R/package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom grDevices rgb
+#' @importFrom importFrom graphics abline axis box legend lines par plot points polygon title
+#' @importFrom stats prcomp predict quantile
+
+#' Data from JAS
+#'
+#' @docType pig
+#' @name pig
+#' @format A `list` containing `gp` (a factor) and `mat` (a `matrix` of dim 171x171).
+#' @source  See citation("mevolCVP")
+NULL
