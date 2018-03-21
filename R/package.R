7c82e9620489d9c5ed40e629b35a7b04d408a005
vbonhomme <bonhomme.vincent@gmail.com>
2018-03-21 15:32:32 +0100

    reorganizing .rds

--- R/package.R
+++ R/package.R
@@ -1,14 +1,12 @@
 #' @keywords internal
 "_PACKAGE"
 
+#' mevolCVP
+#'
 #' @importFrom grDevices rgb
 #' @importFrom graphics abline axis box legend lines par plot points polygon title
 #' @importFrom stats prcomp predict quantile
-
-#' Data from JAS
 #'
-#' @docType pig
-#' @name pig
-#' @format A `list` containing `gp` (a factor) and `mat` (a `matrix` of dim 171x171).
-#' @source  See citation("mevolCVP")
+#' @docType package
+#' @name mevolCVP
 NULL
---
+++ R/pig.R
@@ -0,0 +1,7 @@
+#' Data from JAS
+#'
+#' @docType data
+#' @name pig
+#' @format A `list` containing `gp` (a factor) and `mat` (a `matrix` of dim 171x171).
+#' @source  See citation("mevolCVP")
+NULL
